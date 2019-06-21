8c4064ee2148bdba3cb1da80fdb9eba576133b6e
Jonathan Leary <ollietrickster@gmail.com>
2019-06-21 16:20:11 -0600

    init

---
+++ Model/Songs.js
@@ -0,0 +1,29 @@
+export default class Song {
+  constructor(song) {
+    this.title = song.trackName
+    //Change 250x250 if you want a larger image
+    this.albumArt = song.artworkUrl60.replace(/60x60/g, "250x250")
+    this.artist = song.artistName
+    this.collection = song.collectionName
+    this.price = song.collectionPrice
+    this.preview = song.previewUrl
+  }
+
+
+  get Template() {
+    return `
+    <div class="col-6">
+                    <ul>
+                        <li class= "card" style="width: 18rem;">
+                        <h4>${this.title}</h4>
+                        <img src="${this.albumArt}"></img>
+                        <h4>Artist:${this.artist}</h4>
+                        <h4>Collection:${this.collection}</h4>
+                        <h4>Price:${this.price}</h4>
+                        <audio controls src="${this.preview}"></audio>
+                        </li>
+                </div>
+    `
+
+  }
+}
---
+++ app/components/Itunes/ItunesController.js
@@ -0,0 +1,50 @@
+import ItunesService from "./itunes-service.js";
+//Private
+const itunesService = new ItunesService()
+
+function drawSongs() {
+  document.querySelector('#get-music-button').textContent = 'GET MUSIC'
+  let songsElem = document.querySelector("#songs")
+  let template = ''
+  let songs = itunesService.Songs
+  songs.forEach(song => {
+    template += song.Template
+  })
+  songsElem.innerHTML = template
+  remSongs()
+}
+
+function remSongs() {
+  let remove = []
+}
+
+
+
+//changes button back to GET MUSIC once songs are loaded
+console.log(itunesService.Songs)
+
+
+
+
+
+//PUBLIC
+class ItunesController {
+  constructor() {
+    itunesService.addSubscriber("songs", drawSongs)
+    drawSongs()
+    //BE SURE TO REGISTER YOUR SUBSCRIBERS!!!!!!!
+  }
+
+
+  //DO NOT MODIFY THIS METHOD
+  getMusic(e) {
+    e.preventDefault();
+    var artist = e.target.artist.value;
+    //changes the button to loading while songs load
+    document.querySelector('#get-music-button').textContent = 'LOADING....'
+    itunesService.getMusicByArtist(artist)
+  }
+}
+
+
+export default ItunesController
